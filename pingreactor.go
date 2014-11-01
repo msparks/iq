f76e46d428e4849fac2e7f65afd49266221fd1ff
Matt Sparks <ms@quadpoint.org>
2014-11-01 02:34:46 -0700

    Define PONG message.

--- pingreactor.go
+++ pingreactor.go
@@ -1,6 +1,8 @@
 package main
 
+import "code.google.com/p/goprotobuf/proto"
 import "github.com/msparks/iq/public"
+import ircproto "github.com/msparks/iq/public/irc"
 import "log"
 
 func PingReactor(evs *EventServer) {
@@ -11,7 +13,31 @@ func PingReactor(evs *EventServer) {
 		v := <-notifiee
 		switch v := v.(type) {
 		case *public.Event:
-			log.Printf("PingReactor received event: %+v", v)
+			msg := v.GetIrcMessage(); if msg != nil {
+				if msg.GetMessage().GetType() == ircproto.Message_PING {
+					log.Printf("PingReactor received PING: %+v", msg)
+					pong(msg, evs)
+				}
+			}
 		}
 	}
+}
+
+func pong(msg *public.IrcMessage, evs *EventServer) {
+	target := msg.GetMessage().GetPing().GetTarget()
+	reply := &ircproto.Message{
+		Type: ircproto.Message_PONG.Enum(),
+		Pong: &ircproto.Pong{
+			Target: proto.String(target),
+		},
+	}
+
+	cmd := &public.Command{
+		IrcMessage: &public.IrcMessage{
+			Handle: proto.String(msg.GetHandle()),
+			Message: reply,
+		},
+	}
+
+	log.Printf("Pong: %+v", cmd)
 }
