452d5e1ef6603adb7d01f81459f7c625c5f58b18
Matt Sparks <ms@quadpoint.org>
2014-11-05 02:05:13 -0800

    Add TestFromRWC.

--- ircconnection/ircconnection_test.go
+++ ircconnection/ircconnection_test.go
@@ -2,6 +2,7 @@ package ircconnection
 
 import (
 	. "gopkg.in/check.v1"
+	"net"
 	"testing"
 )
 
@@ -13,9 +14,27 @@ var _ = Suite(&IRCConnectionTest{})
 
 func (s *IRCConnectionTest) TestNewIRCConnection(c *C) {
 	ep := Endpoint{"some server"}
-	conn := NewIRCConnection([]Endpoint{ep})
+	ic := NewIRCConnection([]Endpoint{ep})
 
-	c.Check(conn.State(), Equals, DISCONNECTED)
-	c.Assert(len(conn.Endpoints), Equals, 1)
-	c.Check(conn.Endpoints[0], Equals, ep)
+	c.Check(ic.State(), Equals, DISCONNECTED)
+	c.Assert(len(ic.Endpoints), Equals, 1)
+	c.Check(ic.Endpoints[0], Equals, ep)
+}
+
+func (s *IRCConnectionTest) TestFromRWC(c *C) {
+	// Local server.
+	server, err := net.Listen("tcp", "[::]:0")
+	c.Assert(err, IsNil)
+	defer server.Close()
+	c.Logf("Listening on %s.", server.Addr().String())
+
+	// Connect to local server.
+	conn, err := net.Dial("tcp", server.Addr().String())
+	c.Assert(err, IsNil)
+
+	// Create an IRCConnection from the established connection.
+	ep := Endpoint{"some server"}
+	ic := FromRWC(conn, []Endpoint{ep})
+
+	c.Check(ic.State(), Equals, CONNECTED)
 }
